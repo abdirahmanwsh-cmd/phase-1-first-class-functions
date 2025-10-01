5214b47ecbe0992dd927f54c509bbbad3e86b58c
Manka.code <Abdirahman.wsh@gmail.com>
2025-10-01 00:18:11 -0400

    adds index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(fn) {
+    return fn();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("This is a named function");
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function");
+    };
+}
